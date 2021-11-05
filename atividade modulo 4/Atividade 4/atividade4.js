ef01e02dc10fe7d6ea7c6ee70fbaa2a875c37080
rosanper <rp.santana117@gmail.com>
2021-11-05 14:33:22 -0300

    modificacoes segunda entrega

--- atividade modulo 4/Atividade 4/atividade4.js
+++ atividade modulo 4/Atividade 4/atividade4.js
@@ -1,15 +1,18 @@
-  
 let relogio;
-let tempo;
 let competidor;
-        
+let tempoTotal = 0;
+let centSeg = 0
+let seg = 0
+let min = 0
+
+
+// Pegar Competidor  
 function pegarinput(){
     const formulario = document.querySelector("form")
     const input = document.querySelector("input")
     competidor = input.value
     
     if(competidor){
-        // console.log(competidor)
         formulario.reset()
         return competidor 
     } else{
@@ -17,97 +20,109 @@ function pegarinput(){
     }
    
 }
-        
-    
-function iniciarCronometro(){
-    tempo = 0;
-    relogio = setInterval(()=>{
-        tempo++
-        // console.log(tempo)
-    },100)
-}
-        
-function tempoCorrida(time){
-    let decSeg = time % 10
-    let seg = Math.floor(time / 10)
-    let min = 0 
-
-    if(seg>60){
-        min = Math.floor(seg / 60)
-        seg = seg % 60
-    }
-
-    // console.log(decSeg)
-    // console.log(seg)
-    // console.log(min)
-
-    return {
-        minutos: min, 
-        segundos: seg, 
-        decimosDeSegundo: decSeg
-    }    
-}
-
-function zerarCronometro(){
-    clearInterval(relogio)
-    // console.log(tempo)
-}
-
-
-
-let mostrador = document.querySelector(".mostrador")
-
-function mostraTempo(object){
-    for(let value in object){
-        let t = document.createElement("p")
-        mostrador.appendChild(t)
-        t.innerText = ` ${object[value]} : ${value} `
-    }
-}
 
 function mostrarCompetidor(){
-    let competidorAtual = document.createElement("p")
-    mostrador.appendChild(competidorAtual)
+    let competidorAtual = document.querySelector("h2")
     competidorAtual.innerText = `${competidor}:`
 }
 
-function limparMostrador(){
-    let tempoMostrado = document.querySelectorAll(".mostrador p")
-    console.log(tempoMostrado)
-    for(let ind of tempoMostrado){
-        ind.remove()
+// Cronometro
+
+let minutos = document.querySelector("#minutos")
+let segundos = document.querySelector("#segundos")
+let centesimoDeSegundos = document.querySelector("#centesimoDeSegundos")
+    
+function iniciarCronometro(){
+    relogio = setInterval(()=>{
+        tempoCorrida();
+    },10)
+}  
+
+function tempoCorrida(){
+
+    tempoTotal++
+    centSeg++
+
+    if(centSeg>=100){
+        centSeg = 0
+        seg++
     }
 
+    if(seg>=10){
+        seg = 0
+        min++
+    }
+    
+    mostrarNumeros()
 }
+
+function mostrarNumeros(){
+    minutos.innerText = tempoMostrador(min)
+    segundos.innerText = tempoMostrador(seg)
+    centesimoDeSegundos.innerText = tempoMostrador(centSeg)
+}
+
+function tempoMostrador(value){
+    return value > 10 ? value : `0${value}`
+} 
+ 
+function pararCronometro(){
+    clearInterval(relogio)
+}
+
+function limparMostrador(){
+    tempoTotal = 0;
+    centSeg = 0
+    seg = 0
+    min = 0
+    mostrarNumeros() 
+}
+
+// Adicionar no Storage
+   
+let session = sessionStorage.length === 0 ? localStorage.length + 1: localStorage.length  // => O session foi criado na intenção de que para cada nova sessão fosse adicionado um novo valor no Local Storage
+let campeao;
+let tempoCampeao;
 
 function addSessionStorage(){
-    sessionStorage.setItem(competidor, tempo)
+    sessionStorage.setItem(competidor, tempoTotal)
 }
 
-function addLocalStorage(valor){
-    localStorage.setItem("Vencedor Round 1", valor)
+function addLocalStorage(name,time){
+    let chave = `Vencedor Round ${session}`
+    let valor = {
+        jogador:name,
+        tempo: time,
+        unidadeDeTempo: "Centesimo de segundos"
+    }
+    localStorage.setItem(chave,JSON.stringify(valor))  
 }
 
-function maisrapido(){
-    let campeao = sessionStorage.key(0)
-    let tempoCampeao = sessionStorage.getItem(campeao)
-    for(let i = 0; i < sessionStorage.length ; i++){
-        let adversario = sessionStorage.key(i)
-        let tempoAdversario = sessionStorage.getItem(adversario)
-        if(tempoCampeao > tempoAdversario){
-            campeao = adversario
-            tempoCampeao = tempoAdversario
+function maisRapido(){
+    if(!campeao){
+        campeao = competidor
+        tempoCampeao = tempoTotal
+    }
+    let competidorComparador
+    let tempoComparador
+    for(let i=0; i<sessionStorage.length;i++){
+        competidorComparador = sessionStorage.key(i)
+        tempoComparador = +sessionStorage.getItem(competidorComparador)
+        if(tempoComparador<tempoCampeao){
+            campeao = competidorComparador
+            tempoCampeao = tempoComparador
         }
     }
-    return campeao
 }
+
+// Botões
+
 
 let botaoStart = document.getElementById("start")
 let botaoStop = document.getElementById("stop")
 
-function start(){
-    
 
+function start(){
     limparMostrador()
     let jogador = pegarinput()
     if(jogador){
@@ -117,24 +132,17 @@ function start(){
     
         botaoStart.disabled = true
         botaoStop.disabled = false
-    }
-    
+    }    
+
 }
 
 function stop(){
     
-    zerarCronometro()
-    let tela = tempoCorrida(tempo)
-    mostraTempo(tela)
+    pararCronometro()
     addSessionStorage()
-    let vencedor = maisrapido()
-    // console.log(vencedor)
-    addLocalStorage(vencedor)
+    maisRapido()
+    addLocalStorage(campeao,tempoCampeao)
 
     botaoStart.disabled = false
     botaoStop.disabled = true
 }
-
-
-        
-
--- atividade modulo 4/README.md
+++ atividade modulo 4/README.md
@@ -7,11 +7,9 @@ Essas são as atividades do modulo 4 do curso Santander Coders | Web Full-Stack 
 - Atividade 1 - Calculadora;
 - Atividade 2 - Lista de cadastro;
 - Atividade 3 - Promise;
-- Atividade 4 - Drag race (incompleta)
+- Atividade 4 - Drag race 
 
-Na atividade Promise, para ser gerado a lista demora alguns segundos.
-
-OBS: hoje (02/11), a atividade 4 encontra-se incompleta. Ela já faz tudo que foi solicitada pelo professor, porém é preciso fazer ajustes no armazenamento no Session e Local Storage. 
+OBS: Na atividade Promise, para ser gerado a lista demora alguns segundos.
 
 
 Aluno: Rodrigo Santana Pereira
